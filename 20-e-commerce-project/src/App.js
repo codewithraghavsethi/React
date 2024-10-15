8c4b04313763540bd43fa94e5f7348c4d6601e5b
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-15 09:15:55 +0530

    Code

--- 20-e-commerce-project/src/App.js
+++ 20-e-commerce-project/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Category from './Category';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-600">Hello World</h1>
+      <Category></Category>
     </div>
   );
 }
