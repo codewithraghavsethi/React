6e3c51892176831417f4f0a7ecbb3e8d63bc6a62
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-15 16:43:46 +0530

    Code

--- 20-e-commerce-project/src/App.js
+++ 20-e-commerce-project/src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import Category from './Category';
+import logo from "./logo.svg";
+import "./App.css";
+import Category from "./Category";
 
 function App() {
   return (
     <>
-    <div className='py-[40px]'>
-      <div className='mx-width-[1320px] mx-auto'>
-        <div className='text-center text-[40px] font-bold mb-[30px]'>Our Products</div>
-        <div className='grid grid-cols-[30%_auto] gap-[20px]'>
-          <div>
-            <Category/>
+      <div className="py-[40px]">
+        <div className="max-w-[1320px] mx-auto">
+          <h1 className="text-center text-[40px] font-bold mb-[30px]">
+            Our Products
+          </h1>
+          <div className="grid grid-cols-[30%_auto] gap-[20px]">
+            <div>
+              <Category />
+            </div>
+            <div>
+              <div className="grid grid-cols-3 gap-20">
+                <ProductsItem />
+                <ProductsItem />
+                <ProductsItem />
+                <ProductsItem />
+                <ProductsItem />
+                <ProductsItem />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
 
 export default App;
+
+function ProductsItem() {
+  return (
+    <div className="shadow-lg text-center pb-2">
+      <img
+        src="https://images-static.nykaa.com/creatives/393d3b1e-f813-41e4-a949-a5ea30e01d37/default.jpg?tr=cm-pad_resize,w-900"
+        alt=""
+      />
+      <h4>I Phone</h4>
+    </div>
+  );
+}
--- 20-e-commerce-project/src/Category.jsx
+++ 20-e-commerce-project/src/Category.jsx
@@ -4,6 +4,16 @@ const Category = () => {
   return (
     <div>
         <h3 className='text-[25px] font-[500] p-[10px]'>Product Category</h3>
+
+        <ul>
+          <li className='bg-gray-300 cursor-pointer p-[7px] text-[20px] font-serif font-semibold w-60 mb-2'>Braclate</li>
+          <li className='bg-gray-300 cursor-pointer p-[7px] text-[20px] font-serif font-semibold w-60 mb-2'>Braclate</li>
+          <li className='bg-gray-300 cursor-pointer p-[7px] text-[20px] font-serif font-semibold w-60 mb-2'>Braclate</li>
+          <li className='bg-gray-300 cursor-pointer p-[7px] text-[20px] font-serif font-semibold w-60 mb-2'>Braclate</li>
+          <li className='bg-gray-300 cursor-pointer p-[7px] text-[20px] font-serif font-semibold w-60 mb-2'>Braclate</li>
+          
+        </ul>
+
     </div>
   )
 }
