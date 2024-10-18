b224f2f0e73db0432352dfd88364f3fb2484a85d
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-18 09:51:14 +0530

    Code

--- 22-router-project/src/common/Header.jsx
+++ 22-router-project/src/common/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
     <div>
-      
+      <h1>Header Part</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Course</li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
--- 22-router-project/src/pages/Home.jsx
+++ 22-router-project/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../common/Header'
 
 export default function Home() {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
