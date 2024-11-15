1e77dfcfcf4f7fcfc1cd13bb0c806e76ef755e3a
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-15 11:00:33 +0530

    code

--- 20-react-one-shot/src/App.jsx
+++ 20-react-one-shot/src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
 function App() {
- 
+const [first, setfirst] = useState(second) 
+
   return (
     <>
       <div>hello world</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
