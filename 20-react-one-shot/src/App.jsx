7f7caf9eaeedc9668cc61f37bb35d20ff5bfcda9
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-16 08:57:27 +0530

    code

--- 20-react-one-shot/src/App.jsx
+++ 20-react-one-shot/src/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
-const [first, setfirst] = useState(second) 
+// const [first, setfirst] = useState(second) 
 
   return (
     <>
-      <div>hello world</div>
+      <h1 className="text-blue-500">Raghav</h1>
+      <h2 className="text-red-500">Raghav</h2>
+      <h3 className="text-green-500">Raghav</h3>
+      <h4 className="text-pink-500">Raghav</h4>
+      <h4 className="text-yellow-500">Raghav</h4>
+      <h4 className="text-black">Raghav</h4>
+   
     </>
   );
 }
